bd2b0a610e8115a189591a072cbdd903f634e444
cosyreii <reishi2023@gmail.com>
2025-05-20 13:18:50 +0800

    clean up code

--- src/routes/showcase/+page.svelte
+++ src/routes/showcase/+page.svelte
@@ -1,6 +1,7 @@
 <svelte:head>
 	<title>Reii | Showcase</title>
 </svelte:head>
+
 <p class=" m-4 flex items-center justify-center">
 	Illustrations, models, animations, websites ( Currently too lazy to import it here )
 </p>

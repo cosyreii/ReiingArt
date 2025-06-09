e17b6cca4b9f6800856c04aeb85818ddc3010353
cosyreii <reishi2023@gmail.com>
2025-06-09 11:14:55 +0800

    showcase images

--- src/routes/showcase/+page.svelte
+++ src/routes/showcase/+page.svelte
@@ -7,11 +7,17 @@
 </svelte:head>
 
 <div
-	class="noscrollbar align-center m-1 flex flex-col justify-start overflow-y-scroll px-20 text-left text-lg max-md:px-3"
+	class="noscrollbar align-center m-1 my-4 flex flex-col justify-start overflow-y-scroll px-20 text-left text-lg max-md:px-3"
 >
-	<p class=" m-4 flex items-center justify-center">
-		Illustrations, models, animations, websites ( Currently too lazy to import it here )
-	</p>
+	<div class="grid grid-cols-4  max-lg:grid-cols-3 max-md:grid-cols-1">
+		<img src="https://i.imgur.com/5HpQOMu.png" class="mb-2 w-72" alt="cosyreii logo" />
+		<img src="https://i.imgur.com/tQWb9zt.png" class="mb-2 w-72" alt="butterfly" />
+		<img src="https://i.imgur.com/uqh6nuJ.png" class="mb-2 w-72" alt="sword" />
+		<img src="https://i.imgur.com/BhgWbzb.png" class="mb-2 w-72" alt="plague scythe" />
+		<img src="https://i.imgur.com/hG3YPAh.png" class="mb-2 w-72" alt="soupspoon logo" />
+		<img src="https://i.imgur.com/fbBqyD6.png" class="mb-2 w-72" alt="stride 2025 namecard" />
+		<img src="https://i.imgur.com/rkyf01m.png" class="mb-2 w-72" alt="poxial emotes" />
+	</div>
 
 	<hr class="my-4 w-full border-translucent" />
 
@@ -42,11 +48,7 @@
 			</p>
 		</Review>
 
-		<Review
-			image="https://i.imgur.com/6RPWzpB.png"
-			name="Yukihara"
-			link="/unavailable"
-		>
+		<Review image="https://i.imgur.com/6RPWzpB.png" name="Yukihara" link="/unavailable">
 			<p>
 				I had only vague ideas but she stayed patient and put it into practice way better than I
 				thought it could turn out.

03832bae193b803e93dd6ab994f0e998f9ac17ee
cosyreii <reishi2023@gmail.com>
2025-06-09 17:07:38 +0800

    gallery update

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -10,7 +10,7 @@
 	class="noscrollbar align-center m-1 my-4 flex flex-col justify-center overflow-y-scroll px-20 text-left text-lg max-md:items-center max-md:px-3"
 >
 	<div class="grid grid-cols-4 max-lg:grid-cols-3 max-md:grid-cols-1">
-		<img src="https://i.imgur.com/5HpQOMu.png" class="mb-2 w-72" alt="cosyreii logo" />
+		<img src="https://i.imgur.com/4fIBkTn.png" class="mb-2 w-72" alt="cosyreii logo" />
 		<img src="https://i.imgur.com/fbBqyD6.png" class="mb-2 w-72" alt="stride 2025 namecard" />
 		<img src="https://i.imgur.com/uqh6nuJ.png" class="mb-2 w-72" alt="sword" />
 		<img src="https://i.imgur.com/BhgWbzb.png" class="mb-2 w-72" alt="plague scythe" />

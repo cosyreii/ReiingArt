69ec8d50db62b3caef9f8233c6636f2435cc338e
cosyreii <reishi2023@gmail.com>
2025-06-09 12:12:50 +0800

    more showcase

--- src/routes/showcase/+page.svelte
+++ src/routes/showcase/+page.svelte
@@ -11,12 +11,14 @@
 >
 	<div class="grid grid-cols-4  max-lg:grid-cols-3 max-md:grid-cols-1">
 		<img src="https://i.imgur.com/5HpQOMu.png" class="mb-2 w-72" alt="cosyreii logo" />
-		<img src="https://i.imgur.com/tQWb9zt.png" class="mb-2 w-72" alt="butterfly" />
+		<img src="https://i.imgur.com/fbBqyD6.png" class="mb-2 w-72" alt="stride 2025 namecard" />
+		
 		<img src="https://i.imgur.com/uqh6nuJ.png" class="mb-2 w-72" alt="sword" />
 		<img src="https://i.imgur.com/BhgWbzb.png" class="mb-2 w-72" alt="plague scythe" />
 		<img src="https://i.imgur.com/hG3YPAh.png" class="mb-2 w-72" alt="soupspoon logo" />
-		<img src="https://i.imgur.com/fbBqyD6.png" class="mb-2 w-72" alt="stride 2025 namecard" />
+		<img src="https://i.imgur.com/tQWb9zt.png" class="mb-2 w-72" alt="butterfly" />
 		<img src="https://i.imgur.com/rkyf01m.png" class="mb-2 w-72" alt="poxial emotes" />
+		<img src="https://i.imgur.com/7Uh4P9I.png" class="mb-2 w-72" alt="dupcup 1" />
 	</div>
 
 	<hr class="my-4 w-full border-translucent" />

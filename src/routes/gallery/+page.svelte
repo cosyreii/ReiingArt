812934b1b16826177dfb3a7d830663f6b37485ce
cosyreii <reishi2023@gmail.com>
2025-06-09 12:31:08 +0800

    mobile friendly

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,13 +1,11 @@
-
-
 <svelte:head>
 	<title>Reii | Gallery</title>
 </svelte:head>
 
 <div
-	class="noscrollbar align-center m-1 my-4 flex flex-col justify-start overflow-y-scroll px-20 text-left text-lg max-md:px-3"
+	class="noscrollbar align-center m-1 my-4 flex flex-col justify-center overflow-y-scroll px-20 text-left text-lg max-md:px-3 max-md:items-center"
 >
-	<div class="grid grid-cols-4  max-lg:grid-cols-3 max-md:grid-cols-1">
+	<div class="grid grid-cols-4 max-lg:grid-cols-3 max-md:grid-cols-1">
 		<img src="https://i.imgur.com/5HpQOMu.png" class="mb-2 w-72" alt="cosyreii logo" />
 		<img src="https://i.imgur.com/fbBqyD6.png" class="mb-2 w-72" alt="stride 2025 namecard" />
 		<img src="https://i.imgur.com/uqh6nuJ.png" class="mb-2 w-72" alt="sword" />
@@ -17,5 +15,4 @@
 		<img src="https://i.imgur.com/rkyf01m.png" class="mb-2 w-72" alt="poxial emotes" />
 		<img src="https://i.imgur.com/7Uh4P9I.png" class="mb-2 w-72" alt="dupcup 1" />
 	</div>
-
 </div>

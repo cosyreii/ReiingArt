0f0b31015aa1cd39f16704aa3593537cad307bfe
cosyreii <reishi2023@gmail.com>
2025-06-12 19:31:51 +0800

    gallery update - reii, suchi, levhanni (ref sheets)

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -14,10 +14,14 @@
 		<img src="https://i.imgur.com/fbBqyD6.png" class="mb-2 w-72" alt="stride 2025 namecard" />
 		<img src="https://i.imgur.com/uqh6nuJ.png" class="mb-2 w-72" alt="sword" />
 		<img src="https://i.imgur.com/BhgWbzb.png" class="mb-2 w-72" alt="plague scythe" />
-		<img src="https://i.imgur.com/hG3YPAh.png" class="mb-2 w-72" alt="soupspoon logo" />
 		<img src="https://i.imgur.com/tQWb9zt.png" class="mb-2 w-72" alt="butterfly" />
+		<img src="https://i.imgur.com/qKLKYlH.png" class="mb-2 w-72" alt="cosyreii" />
+		<img src="https://i.imgur.com/hG3YPAh.png" class="mb-2 w-72" alt="soupspoon logo" />
 		<img src="https://i.imgur.com/US3BGdd.png" class="mb-2 w-72" alt="poxial emotes" />
 		<img src="https://i.imgur.com/7Uh4P9I.png" class="mb-2 w-72" alt="dupcup 1" />
+		<img src="https://i.imgur.com/92SQ8Aa.png" class="mb-2 w-72" alt="noctovilia suchi" />
+		<img src="https://i.imgur.com/PnB042t.png" class="mb-2 w-72" alt="noctovilia details" />
+		<img src="https://i.imgur.com/5L9Kr5g.png" class="mb-2 w-72" alt="levhanni" />
 	</div>
 </div>
 

aaf9c1937a2545e188cd0ade1e2dee5f91cad0f3
cosyreii <reishi2023@gmail.com>
2025-07-23 11:48:38 +0800

    youtube embed

--- src/routes/arkadia/+page.svelte
+++ src/routes/arkadia/+page.svelte
@@ -63,11 +63,46 @@
 				</div>
 
 				<p>
-					<span class="font-bold">GOAL</span> : To encourage drinking ( natural ) fruit juice instead of overly sweet beverages such
-					as soda.
+					<span class="font-bold">GOAL</span> : To encourage drinking ( natural ) fruit juice instead
+					of overly sweet beverages such as soda.
 				</p>
 			</div>
 		</div>
+	</div>
+	<div class="mt-6 flex flex-row">
+		<div class="m-2 flex w-1/3 flex-col justify-center">
+			<div class="flex flex-col items-center justify-center">
+				<p class="text-2xl font-black text-pink">Softwares</p>
+				<div class="flex flex-row">
+					<img src="https://i.imgur.com/h4AEg0K.png" class="mr-2 size-6" alt="CSP" />
+					<p class="text-xl">Clip Studio Paint Ex</p>
+				</div>
+				<div class="flex flex-row">
+					<p class="text-xl">Adobe After Effects</p>
+					<img src="https://i.imgur.com/hPKoqBX.png" class="ml-2 size-6" alt="CSP" />
+				</div>
+
+				<iframe
+					src="https://www.youtube.com/embed/a4Y0Hiz27Lw?si=0nJAuuu2xeq_uL5s"
+					title="YouTube video player"
+					frameborder="0"
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+					referrerpolicy="strict-origin-when-cross-origin"
+					allowfullscreen
+					class="mt-8 h-[180px] w-[320px] rounded-3xl"
+				></iframe>
+				<p class="text-translucent">Shortform Advertisement (15s)</p>
+			</div>
+		</div>
+		<iframe
+			src="https://www.youtube.com/embed/a4Y0Hiz27Lw?si=0nJAuuu2xeq_uL5s"
+			title="YouTube video player"
+			frameborder="0"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+			referrerpolicy="strict-origin-when-cross-origin"
+			allowfullscreen
+			class="h-[480px] w-[854px] rounded-3xl"
+		></iframe>
 	</div>
 </div>
 
